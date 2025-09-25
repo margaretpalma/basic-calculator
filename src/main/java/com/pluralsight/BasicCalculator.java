9af2c24883dc8a96c5a0a0ea300e45f203ebfe1b
margaretpalma <mpalma1496@gmail.com>
2025-09-25 15:19:06 -0400

    inital commit

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -1,11 +1,11 @@
 package com.pluralsight;
 
-import java.util.Scanner;.java.util.Scanner;  //importing the scanner class from java package, to read input from the keyboard
+import java.util.Scanner; //importing the scanner class from java package, to read input from the keyboard
 
 public class BasicCalculator {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in)  //scanner reads from the keyboard
+        Scanner scanner = new Scanner(System.in); //scanner reads from the keyboard
 
         System.out.println("Enter the first number: ");  //asking for first number
         double firstNumber = scanner.nextDouble();     //asking for two floating point numbers so you use a double
@@ -14,10 +14,14 @@ public class BasicCalculator {
         double secondNumber = scanner.nextDouble();
 
         System.out.println("Choose one: Addition, Subtraction, Multiplication, or Division? ");
-        String operation = scanner.next()  //string operation is whatever you type goes into a variable named operation
-                //String because the words are strings not numbers
+        String operation = scanner.next();  //string operation is whatever you type goes into a variable named operation
+        //String because the words are strings not numbers
 
         double result = firstNumber * secondNumber;
+
+        System.out.printf("The result is: %.2f%n", result);
+
+        scanner.close();  //have to close the scanner
 
 
     }
