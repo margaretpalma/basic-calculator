0c9db6ef31850f6fb414ac41c7bba39baf04e183
margaretpalma <mpalma1496@gmail.com>
2025-09-25 14:55:22 -0400

    inital commit

---
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import java.util.Scanner;.java.util.Scanner;  //importing the scanner class from java package, to read input from the keyboard
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in)  //scanner reads from the keyboard
+
+        System.out.println("Enter the first number: ");
+        double first Number = scanner.nextDouble();     //asking for two floating point numbers so you use a double
+
+
+
+    }
+}
